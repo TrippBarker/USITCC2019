5717f10417e538ec9737652b4a16ab433db107eb
Tripp Barker <colemanbarker@gmail.com>
2023-03-25 21:02:42 -0400

    working on regex

--- src/application/RegisterSceneController.java
+++ src/application/RegisterSceneController.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class RegisterSceneController {
 	@FXML
@@ -30,5 +32,39 @@ public class RegisterSceneController {
 	
 	public void cancelRegister(ActionEvent e) throws IOException {
 		Main.ss.switchScene(Main.LOGIN_SCENE_PATH);
+	}
+	
+	public void checkValidEntry(KeyEvent e) {
+		TextField source = (TextField)e.getSource();
+		String regex = "[^a-zA-Z]";
+		int maxLen = 16;
+		switch (source.getId()) {
+		case "fNameField":
+			break;
+		case "mInitialField":
+			regex = "[^A-Z]";
+			maxLen = 1;
+			break;
+		case "lNameField":
+			break;
+		case "streetField":
+			regex = "[^a-zA-Z0-9]";
+			maxLen = 30;
+			break;
+		case "cityField":
+			break;
+		case "zipField":
+			regex = "[^0-9]";
+			maxLen = 5;
+			break;
+		}
+		
+		source.setText(source.getText().replaceAll(regex, ""));
+		if (source.getText().length() > maxLen) {
+			source.setText(source.getText().substring(0, maxLen));
+		}
+		source.positionCaret(source.getText().length());
+		
+		
 	}
 }
