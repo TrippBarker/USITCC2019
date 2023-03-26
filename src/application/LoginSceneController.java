bc6b1adbd0a59212741fb3de3dd3351437cbe2a7
Tripp Barker <colemanbarker@gmail.com>
2023-03-26 00:24:23 -0400

    working on controller

--- src/application/LoginSceneController.java
+++ src/application/LoginSceneController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class LoginSceneController {
 	
@@ -29,4 +30,31 @@ public class LoginSceneController {
 	public void RegisterNewUser() throws IOException {
 		Main.ss.switchScene(Main.REGISTER_SCENE_PATH);
 	}
+	
+	
+	public void checkValidEntry(KeyEvent e) {
+		TextField source = (TextField)e.getSource();
+		int caretPos = source.getCaretPosition();
+		String regex = "[^a-zA-Z]";
+		int maxLen = 16;
+		switch (source.getId()) {
+		case "usernameField":
+			regex = "[^a-zA-Z0-9]";
+			maxLen = 10;
+			break;
+		case "passwordField":
+			regex = "[^a-zA-Z0-9!@#$%&*_-]";
+			maxLen = 20;
+			break;
+		}
+		
+		source.setText(source.getText().replaceAll(regex, ""));
+		if (source.getText().length() > maxLen) {
+			source.setText(source.getText().substring(0, maxLen));
+		}
+		source.positionCaret(caretPos);
+	}
+	
 }
+
+
